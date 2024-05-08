b0134887d042a1e7335e7160aabedbc6b543591d
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-05-08 18:20:09 +0500

    crud application

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,26 +1,54 @@
-import 'dart:convert';
+import 'dart:convert' as converter;
 import 'package:crudapp/ModelCrud.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class todo extends StatefulWidget {
-  const todo({super.key});
+  const todo({Key? key}) : super(key: key);
 
   @override
-  State<todo> createState() => _MyWidgetState();
+  State<todo> createState() => _TodoState();
 }
 
-class _MyWidgetState extends State<todo> {
-  getPostapi() async {
+class _TodoState extends State<todo> {
+  List<ModelCrud> task = [];
+
+  Future<List<ModelCrud>> getPostapi() async {
     var url = Uri.parse(
         "https://crudcrud.com/api/a1c5014e53294f12b926bbfddc21c83d/unicorns");
     var response = await http.get(url);
-    var responseBody = jasonDecode(response.Body);
-    return ModelCrud.fromJson(responseBody);
+    var responseBody = converter.jsonDecode(response.body); // Fixed typo here
+    var eachmap;
+    for (eachmap in responseBody) {
+      task.add(ModelCrud.fromJson(eachmap));
+    }
+    return task;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: getPostapi(),
+        builder: (context, AsyncSnapshot<List<ModelCrud>> snapshot) {
+          if (snapshot.hasData) {
+            // Fixed typo here
+            return ListView.builder(
+              itemCount: snapshot.data!.length, // Fixed typo here
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].taskName.toString() ??
+                      "no taskname"), // Fixed typo here
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
