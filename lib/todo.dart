658e7dff9c0a661315539681cf20ea9a8c4f6e79
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-05-08 17:49:38 +0500

    crud application

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:crudapp/ModelCrud.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class todo extends StatefulWidget {
   const todo({super.key});
@@ -8,8 +11,16 @@ class todo extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<todo> {
+  getPostapi() async {
+    var url = Uri.parse(
+        "https://crudcrud.com/api/a1c5014e53294f12b926bbfddc21c83d/unicorns");
+    var response = await http.get(url);
+    var responseBody = jasonDecode(response.Body);
+    return ModelCrud.fromJson(responseBody);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
