175a798800c24e50cb24b6d12ef9591f2b69fc15
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-05-09 19:03:12 +0500

    crud application

--- lib/todo.dart
+++ lib/todo.dart
@@ -11,6 +11,7 @@ class todo extends StatefulWidget {
 }
 
 class _TodoState extends State<todo> {
+  DateTime now = DateTime.now();
   List<ModelCrud> task = [];
 
   Future<List<ModelCrud>> getPostapi() async {
@@ -37,17 +38,84 @@ class _TodoState extends State<todo> {
               itemCount: snapshot.data!.length, // Fixed typo here
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(snapshot.data![index].name.toString() ??
-                      "no taskname"), // Fixed typo here
+                  tileColor: Colors.blueGrey,
+                  leading: const CircleAvatar(
+                    backgroundColor: Colors.black,
+                  ),
+                  title:
+                      Text(snapshot.data![index].name.toString() ?? "no name"),
+                  subtitle: Row(
+                    children: [
+                      Text("${snapshot.data![index].age.toString()} years" ??
+                          "no age"),
+                      Text(' ${now.day}-${now.month}-${now.year}'),
+                    ],
+                  ),
                 );
               },
             );
           } else {
             return const Center(
-              child: CircularProgressIndicator(),
+              child: Text(
+                "NOTHING TO SHOW ANY DATA",
+                style: TextStyle(color: Colors.white),
+              ),
             );
           }
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              TextEditingController nameController = TextEditingController();
+              TextEditingController ageController = TextEditingController();
+              return AlertDialog(
+                title: const Text("Enter Your Details"),
+                content: Column(
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(hintText: "Name"),
+                    ),
+                    TextField(
+                      controller: ageController,
+                      decoration: const InputDecoration(hintText: "Age"),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        String name = nameController.text;
+                        String age = ageController.text;
+                        SendDatatoapi(name, age);
+                        nameController.clear();
+                      });
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Add"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Text(
+          "Add",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
